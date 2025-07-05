0c1792571356b8c01ba2cbed462ec88ac3b4577c
Rimuru <volokitin11@internet.ru>
2025-07-05 20:35:06 +0700

    переделал функцию getzapros

--- get.py
+++ get.py
@@ -1,18 +1,24 @@
-from openai import AsyncOpenAI
+from aiohttp import *
+import asyncio
 
-client = AsyncOpenAI(
-  base_url="https://openrouter.ai/api/v1",
-  api_key="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",
-)
-async def getzapros(text):
-    completion = await client.chat.completions.create(
-    model="deepseek/deepseek-chat:free",
-    messages=[
-        {
-        "role": "user",
-        "content": f"{text}"
-        }
-    ]
-    )
-    print(completion.choices[0].message.content)
-    return completion.choices[0].message.content
+async def getzapros():
+    async with ClientSession() as session:
+        async with session.post(
+            url="https://openrouter.ai/api/v1/chat/completions",
+            headers={
+                "Authorization": "Bearer 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",
+                "Content-Type": "application/json",
+            },
+            json={
+                "model": "deepseek/deepseek-r1-distill-llama-70b:free",
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": "What is the meaning of life?"
+                    }
+                ],
+            },
+        ) as response:
+            data = await response.json()
+            print(data["choices"][0]["message"]["content"])
+            return data["choices"][0]["message"]["content"]
